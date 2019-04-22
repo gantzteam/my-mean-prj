84fcd09fba9b5cba3465aac5aac38b58cbdc1b68
KiTT <pjitako@medicine.psu.ac.th>
2019-04-22 14:59:50 +0700

    test connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+// 9eDxEXP0KjZFvs3q
+mongoose
+  .connect(
+    'mongodb+srv://kit:dqtfAXOL0RQ6f1Eq@cluster0-jdlxh.mongodb.net/test?retryWrites=true',
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   //console.log('First middleware');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    'Access-Control-Allow-Header',
+    'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader(
@@ -16,7 +35,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: 'asdasdsadas',
